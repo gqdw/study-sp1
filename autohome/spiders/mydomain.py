cbcacd8ce91bb631d57d7273097a5c592895d5a2
gqdw <aca_jingru@qq.com>
2016-05-22 22:06:48 +0800

    download website with scrapy spider

--- autohome/spiders/mydomain.py
+++ autohome/spiders/mydomain.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+class MydomainItem(scrapy.Item):
+	title = scrapy.Field()
+	link = scrapy.Field()
+	desc = scrapy.Field()
+
 
 class MydomainSpider(scrapy.Spider):
     name = "mydomain"
-    allowed_domains = ["mydomain.com"]
+    allowed_domains = ["dmoz.com"]
     start_urls = (
-        'http://www.mydomain.com/',
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    
     )
 
     def parse(self, response):
-        pass
+		filename = response.url.split("/")[-2] + '.html'
+		with open(filename, 'wb') as f:
+			f.write(response.body)
