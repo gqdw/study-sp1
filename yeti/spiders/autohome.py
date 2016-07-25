c707cde39887141c5d18ef5dd8916a32f8c73d28
gqdw <aca_jingru@qq.com>
2016-07-25 12:02:56 +0800

    change sth

--- yeti/spiders/autohome.py
+++ yeti/spiders/autohome.py
@@ -14,23 +14,27 @@ class AutohomeSpider(scrapy.Spider):
 #        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     
     )
-
 # <a class="a_topic" target="_blank" href="/bbs/thread-c-3013-54488086-1.html">
 # Yeti获2015中国小型SUV金方向盘奖            </a>
+#	def parse(self, response):
+#		print response.selector.xpath('//dt/a').extract()
     def parse(self, response):
-		# for sel in response.xpath('//ul/li'):
 		for sel in response.selector.xpath('//dt/a'):
-		# for sel in response.selector.xpath('//dt/a').extract():
-		# for sel in response.selector.xpath('//dt/a/text()').extract():
+			print sel.xpath('text()').extract()
+			print sel.xpath('@href').extract()
+#		# for sel in response.xpath('//ul/li'):
+##		filename = response.url.split("/")[-2] + '.html'
+#		for sel in response.selector.xpath('//dt/a'):
+#		# for sel in response.selector.xpath('//dt/a').extract():
+#		# for sel in response.selector.xpath('//dt/a/text()').extract():
 #			print sel
-			item = AutohomeItem()
-			item['title'] = sel.xpath('//a/text()').extract()
-			print item['title']
-			item['link'] = sel.xpath('//a/@href').extract()
-			print item['link']
-#			item['desc'] = sel.xpath('text()').extract()
-			yield item
+#			item = AutohomeItem()
+#			item['title'] = sel.xpath('.a/text()').extract()
+##			print item['title']
+#			item['link'] = sel.xpath('.a/@href').extract()
+##			print item['link']
+##			item['desc'] = sel.xpath('text()').extract()
+##			with open(filename, 'wb') as f:
+##				f.write(item['title'])
+#			yield item
 
-		# filename = response.url.split("/")[-2] + '.html'
-		# with open(filename, 'wb') as f:
-			# f.write(response.body)
